482766c74e6c714144acadda34f65d5d3e642b93
CombatWombatZockchster <56310052+CombatWombatZockchster@users.noreply.github.com>
2020-01-22 17:31:28 +0100

    dimensions hotfix! wichtig

--- GoFast/Assets/Scripts/Interaction/DSwitchable.cs
+++ GoFast/Assets/Scripts/Interaction/DSwitchable.cs
@@ -12,31 +12,46 @@ using UnityEngine;
 
 public class DSwitchable : MonoBehaviour
 {
-    private Material[] myMaterial;
-    private int[] myLayer = new int[0];
-    private UnityEngine.Rendering.ShadowCastingMode[] castShadow = new UnityEngine.Rendering.ShadowCastingMode[0];
     public int dimensionIndex = 0;//only used for initialisation
     private Dimension dimension;
 
-    private Renderer[] render = new Renderer[0];
 
+    GameObject[] children = new GameObject[0];
+
+    private Renderer[] render = new Renderer[0];
+    private UnityEngine.Rendering.ShadowCastingMode[] castShadow = new UnityEngine.Rendering.ShadowCastingMode[0];
+
+    private List<Material[]> myMaterial = new List<Material[]>();
+    private List<Material[]> dimMaterial = new List<Material[]>();
+
+    private int[] myLayer = new int[0];
+    private int layer = 0;
+
+    
     private void Awake()//Initiallize before intial switch
     {
+        dimension = DSwitcher.register(this);
+        if (dimension == null) Debug.LogWarning(name + " failed to register to " + dimensionIndex);
+        else Debug.Log(name + " registered to dimension " + dimension.index);
+
+
+        Transform[] childTrans = gameObject.GetComponentsInChildren<Transform>();
+        children = new GameObject[childTrans.Length];
+        for (int i = 0; i < children.Length; i++)
+        {
+            children[i] = childTrans[i].gameObject;
+        }
 
         if (render.Length == 0) render = GetComponentsInChildren<Renderer>();
         if (render.Length == 0) Debug.LogError("There is no valid renderer for " + name);
 
-        myMaterial = new Material[render.Length];
-        for (int i = 0; i < render.Length; i++)
-        {
-            myMaterial[i] = render[i].material;
-        }
 
-        myLayer = new int[render.Length];
-        for (int i = 0; i < render.Length; i++)
+        myLayer = new int[children.Length];
+        for (int i = 0; i < children.Length; i++)
         {
-            myLayer[i] = render[i].gameObject.layer;
+            myLayer[i] = children[i].layer;
         }
+        layer = gameObject.layer;
 
         castShadow = new UnityEngine.Rendering.ShadowCastingMode[render.Length];
         for (int i = 0; i < render.Length; i++)
@@ -44,9 +59,16 @@ public class DSwitchable : MonoBehaviour
             castShadow[i] = render[i].shadowCastingMode;
         }
 
-        dimension = DSwitcher.register(this);
-        if (dimension == null) Debug.LogWarning(name + " failed to register to " + dimensionIndex);
-        else Debug.Log(name + " registered to dimension " + dimension.index);
+        for (int i = 0; i < render.Length; i++)
+        {
+            myMaterial.Add(render[i].materials);
+            Material[] mat = new Material[render[i].materials.Length];
+            for (int j = 0; j < mat.Length; j++)
+            {
+                mat[j] = dimension.mat;
+            }
+            dimMaterial.Add(mat);
+        }
     }
 
     public void switchTo(int dim)
@@ -57,18 +79,33 @@ public class DSwitchable : MonoBehaviour
             {
                 if (dimension.index != dim)
                 {
-                    render[i].material = dimension.mat;
-                    render[i].gameObject.layer = 9;//pass through
+                    //Debug.Log(i + ", " + render.Length + ", " + dimMaterial.Count);
+                    render[i].materials = dimMaterial[i];
                     render[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
                 }
                 else
                 {
-                    render[i].material = myMaterial[i];
-                    render[i].gameObject.layer = myLayer[i];
+                    render[i].materials = myMaterial[i];
                     render[i].shadowCastingMode = castShadow[i];
+                }
+            }
+
+            for (int i = 0; i < children.Length; i++)
+            {
+                if (dimension.index != dim)
+                {
+                    children[i].gameObject.layer = 9; //passthrough
+                    gameObject.layer = 9;
+                }
+                else
+                {
+                    children[i].gameObject.layer = myLayer[i];
+                    gameObject.layer = layer;
                 }
             }
         }
         else Debug.LogWarning(name + " tried to switch dimension, but its own dimension is null");
     }
 }
+//TODO: support multiple materials per mesh
+//also change layer of all objects, not just one
